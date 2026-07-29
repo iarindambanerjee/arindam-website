8fd2b5073db0d0d7a69499bfd87d941d28a16ae9
Arindam Banerjee <iarindambanerjee@gmail.com>
2026-07-29 13:08:18 +0530

    Temporarily disable Services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,19 +1,5 @@
-import Hero from "@/components/services/Hero";
-import Clients from "@/components/services/Clients";
-import Services from "@/components/services/Services";
-import WhyChooseMe from "@/components/services/WhyChooseMe";
-import EngagementProcess from "@/components/services/EngagementProcess";
-import CTA from "@/components/services/CTA";
+import { notFound } from "next/navigation";
 
 export default function ServicesPage() {
-  return (
-    <main className="bg-white text-slate-900">
-      <Hero />
-      <Clients />
-      <Services />
-      <WhyChooseMe />
-      <EngagementProcess />
-      <CTA />
-    </main>
-  );
+  notFound();
 }
